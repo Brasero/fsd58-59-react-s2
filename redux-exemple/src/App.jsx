79acbdaac4b3eb83aabf74d2a7e70aae182724fe
Brasero <brandonricci4@gmail.com>
2025-02-27 10:05:36 +0100

    presentation de toolkit

--- redux-exemple/src/App.jsx
+++ redux-exemple/src/App.jsx
@@ -1,28 +1,12 @@
 import './App.css'
-import DragonPage from "./page/Dragon/index.page.jsx";
-import LogPage from "./page/Log/index.page.jsx";
-import KnightPage from "./page/Knight/index.page.jsx";
-import CouplePage from "./page/Couple/index.page.jsx";
-import {useDispatch, useSelector} from "react-redux";
-import {selectCount, startCounter} from "./store/reducer/CounterReducer.jsx";
+import MessageForm from "./component/MessageForm/index.jsx";
 
 function App() {
- 
- const count = useSelector(selectCount)
- const dispatch = useDispatch()
- 
- const onClick = () => {
-  dispatch(startCounter())
- }
+
  
   return (
     <>
-     <span>{count}</span>
-     <button onClick={onClick}>Start</button>
-     <LogPage />
-     <DragonPage />
-     <KnightPage />
-     <CouplePage />
+      <MessageForm />
     </>
   )
 }
--- redux-exemple/src/store/index.js
+++ redux-exemple/src/store/index.js
@@ -1,26 +1,10 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware} from "redux"
-import dragonReducer from "./reducer/dragonReducer.js";
-import logReducer from "./reducer/logReducer.js";
-import knightReducer from "./reducer/knightReducer.js";
-import coupleReducer from "./reducer/coupleReducer.jsx";
-import {logMiddleware} from "./middleware/logMiddleware.js";
-import {composeWithDevTools} from "@redux-devtools/extension"
-import counterReducer from "./reducer/CounterReducer.jsx";
-import {thunk} from "redux-thunk";
+import {configureStore} from "@reduxjs/toolkit";
+import messageSlice from "./messageSlice.js";
 
-
-const store = createStore(
- combineReducers({
-  dragons: dragonReducer,
-  logs: logReducer,
-  knights: knightReducer,
-  couple: coupleReducer,
-  counter: counterReducer
- }),
- composeWithDevTools(
-  applyMiddleware(logMiddleware, thunk)
- )
-)
-
+const store = configureStore({
+	reducer: {
+		messages: messageSlice
+	}
+})
 
 export default store
